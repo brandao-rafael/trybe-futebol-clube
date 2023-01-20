f9895e8a6b392d929e855235b053a3cb2d9de075
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-01-20 17:11:19 -0300

    model user fail

--- app/backend/src/database/models/index.ts
+++ app/backend/src/database/models/index.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from 'sequelize';
 import * as config from '../config/database';
+import Users from './users.model';
 
 const sequelize = new Sequelize(config)
 
 export default sequelize;
+export { Users }
---
+++ app/backend/src/database/models/users.model.ts
@@ -0,0 +1,43 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+export default class Users extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+Users.init(
+  {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+      type: INTEGER,
+    },
+    username: {
+      allowNull: false,
+      type: STRING,
+    },
+    role: {
+      allowNull: false,
+      type: STRING,
+    },
+    email: {
+      allowNull: false,
+      type: STRING,
+    },
+    password: {
+      allowNull: false,
+      type: STRING,
+    },
+  },
+  {
+    modelName: 'users',
+    underscored: true,
+    timestamps: false,
+    sequelize: db,
+  },
+);
