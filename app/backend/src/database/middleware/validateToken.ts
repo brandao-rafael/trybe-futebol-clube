a62775f30292f2923d0d166c8c2a80fb8fe42250
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-01-24 16:46:20 -0300

    Feat: its impossible init a match with an invalid token

--- app/backend/src/database/middleware/validateToken.ts
+++ app/backend/src/database/middleware/validateToken.ts
@@ -10,10 +10,13 @@ const validateToken = (req: Request, res: Response, next: NextFunction) => {
   const { authorization } = req.headers;
 
   if (!authorization) return res.status(401).json({ message: 'Token not found' });
-
-  const tokenVerified = jwt.verify(authorization, JWT_SECRET);
-  req.body.user = tokenVerified;
-  next();
+  try {
+    const tokenVerified = jwt.verify(authorization, JWT_SECRET);
+    req.body.user = tokenVerified;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Token must be a valid token' });
+  }
 };
 
 export default validateToken;
