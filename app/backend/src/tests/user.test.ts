2fdd7cb1f50b29b0c1a62691848ff161b0b1c3bc
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-01-26 18:19:16 -0300

    add some tests

---
+++ app/backend/src/tests/user.test.ts
@@ -0,0 +1,50 @@
+// import * as sinon from 'sinon';
+import * as chai from 'chai';
+// import * as jwt from 'jsonwebtoken';
+// import { Response } from 'superagent';
+const chaiHttp = require('chai-http');
+// import Users from '../database/models/users.model';
+import { App } from '../app';
+
+import {
+  noEmail,
+  noPassword,
+  wrongEmail,
+  wrongPassword,
+} from './mocks/user.mock';
+
+chai.use(chaiHttp);
+
+const { app } = new App();
+
+const { expect } = chai;
+
+describe('test if route login have an expected behavior', () => {
+  it('is impossible siginIn without password', async () => {
+    const result = await chai.request(app).post('/login').send(noPassword);
+
+    expect(result.body).to.be.deep.equal({ message: 'All fields must be filled'});
+    expect(result.status).to.be.equal(400);
+  });
+
+  it('is impossible siginIn without email', async () => {
+    const result = await chai.request(app).post('/login').send(noEmail);
+
+    expect(result.body).to.be.deep.equal({ message: 'All fields must be filled'});
+    expect(result.status).to.be.equal(400);
+  });
+
+  it('is impossible siginIn with wrong email', async () => {
+    const result = await chai.request(app).post('/login').send(wrongEmail);
+
+    expect(result.body).to.be.deep.equal({ message: 'All fields must be filled'});
+    expect(result.status).to.be.equal(400);
+  });
+
+  it('is impossible siginIn without email', async () => {
+    const result = await chai.request(app).post('/login').send(wrongPassword);
+
+    expect(result.body).to.be.deep.equal({ message: 'All fields must be filled'});
+    expect(result.status).to.be.equal(400);
+  });
+})
